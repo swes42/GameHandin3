4f05402694509467ee64cac47a6d1d7322ada913
Selina <soendengaard@hotmail.com>
2021-04-14 23:47:28 +0200

    added a player, but im not happy that i didnt get the animations to work.. I also tried to make a healt bar, but not finished

---
+++ Assets/Selina_Player/Scripts/ThirdPersonMovement.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThirdPersonMovement : MonoBehaviour
+{
+    public CharacterController controller;
+    public Transform cam;
+
+    //hastigheds variabel
+    public float speed = 6f;
+
+    public float turnSmoothSpeed = 0.1f;
+    float turnSmoothVelocity;
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+        Vector3 dir = new Vector3(horizontal, 0f, vertical).normalized;
+
+        //tjekker om vi kan rykke i hvilken somhelst retning
+        if (dir.magnitude >= 0.1f)
+        {
+            //Får spilleren til at vende ansigtet mod den retning han går
+            float targetAngle = Mathf.Atan2(dir.x, dir.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
+            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothSpeed);
+            transform.rotation = Quaternion.Euler(0f, angle, 0f);
+
+            Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
+            controller.Move(moveDir.normalized * speed * Time.deltaTime);
+            
+        }
+    }
+}
